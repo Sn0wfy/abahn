bf3cb3d6bc17659ff5de1534aec02f8eb0a99515
Sn0wfy <maincoon91@gmail.com>
2025-07-18 09:45:57 +0200

    Create leaflet.awesome-markers.js

---
+++ assets/leaflet.awesome-markers.js
@@ -0,0 +1,19 @@
+
+/*!
+ * Leaflet.awesome-markers JS
+ * https://github.com/lvoogdt/Leaflet.awesome-markers
+ */
+L.AwesomeMarkers = {};
+L.AwesomeMarkers.Icon = L.Icon.extend({
+    options: {
+        iconSize: [35, 45],
+        iconAnchor: [17, 42],
+        popupAnchor: [1, -32],
+        prefix: 'fa',
+        spinClass: 'fa-spin',
+        extraClasses: '',
+        icon: 'home',
+        markerColor: 'blue',
+        iconColor: 'white'
+    }
+});
